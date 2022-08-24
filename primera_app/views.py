85245e2b1413357395283b2278e907845ede5df7
JulioSalazar <julio522002@gmail.com>
2022-08-24 13:21:56 -0400

    completado view familiar con template

--- primera_app/views.py
+++ primera_app/views.py
@@ -6,9 +6,22 @@ from .models import *
 
 
 def familiar(request):
-    familiar_1 = Familia(nombre = "Julio", apellido = "Salazar", edad = 20, fecha_nacimiento = 2002-2-5)
+    familiar_1 = Familia(nombre = "Julio", apellido = "Salazar", edad = "20", fecha_nacimiento = "2002-2-5")
+    familiar_2 = Familia(nombre = "Paola", apellido = "Salazar", edad = "23", fecha_nacimiento = "1999-9-18")
+    familiar_3 = Familia(nombre = "Julio", apellido = "Salazar", edad = "50", fecha_nacimiento = "1972-12-23")
+    familiar_4 = Familia(nombre = "Mercedes", apellido = "Salazar", edad = "48", fecha_nacimiento = "1974-11-17")
     # familiar_1.save()
-    diccionario = {"familiar_1" : familiar_1}
+    # familiar_2.save()
+    # familiar_3.save()
+    # familiar_4.save()
+
+    diccionario = {
+        "familiar_1" : familiar_1,
+        "familiar_2" : familiar_2,
+        "familiar_3" : familiar_3,
+        "familiar_4" : familiar_4,
+        }
+        
     plantilla = loader.get_template("familiares.html")
     documento = plantilla.render(diccionario)
     return HttpResponse(documento)
